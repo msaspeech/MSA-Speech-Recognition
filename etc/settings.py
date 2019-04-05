c31f49cb0815a566f8be5f077d0a0bab096d4b9a
yasminehadjeres <yasmine.hadjeres@gmail.com>
2019-04-05 03:46:37 +0100

    Added hard coded needed paths (commit_in_a_hurry)

---
+++ etc/settings.py
@@ -0,0 +1,4 @@
+# TODO : Remove hard coded paths
+QCRI_CORPUS_PATH = '/Users/slash/Desktop/QCRI Corpus/train_wav/'
+OTA_CORPUS_PATH = '/Users/slash/Desktop/OTA Corpus/arabic-speech-corpus/wav/'
+TRANSCRIPTS_PATH = '/Users/slash/Desktop/OTA Corpus/arabic-speech-corpus/orthographic-transcript.txt'
