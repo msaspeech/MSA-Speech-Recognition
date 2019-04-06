7b3b1056fde08288f5c7fbbe74ec421fb2f76550
sofiane_mdjk <sofiane.medjkoune@protonmail.com>
2019-04-06 22:50:59 +0100

    Preparing dataset for input data

---
+++ lib/data_preprocessing/prepare_dataset_pad.py
@@ -0,0 +1,27 @@
+from utils import upload_data_after_padding
+import pandas as pd
+
+def get_input_data(train_ratio=0.8):
+    train_x, test_x = list(), list()
+    data = upload_data_after_padding()
+    train_length = int(len(data) * train_ratio)
+    for i, audio_sample in enumerate(data):
+        if i <= train_length:
+            train_x.append(audio_sample.mfcc)
+            train_x = pd.DataFrame.from_records(train_x)
+        else:
+            test_x.append(audio_sample.mfcc)
+            test_x = pd.DataFrame.from_records(test_x)
+    return train_x, test_x
+
+
+def get_output_data():
+
+    return None, None
+
+
+def upload_dataset():
+    train_x, test_x = get_input_data()
+    train_y, test_y = get_output_data()
+
+
--- main.py
+++ main.py
@@ -2,4 +2,6 @@ from lib import generate_fixed_size_data
 from utils import upload_data_after_padding
 import numpy as np
 
+from lib import get_input_data
 
+train_x, test_x = get_input_data()
