d00aaa41e13f4717d9c5b6f411bfacbd9b3c9f3f
sofiane_mdjk <sofiane.medjkoune@protonmail.com>
2019-04-11 20:38:59 +0100

    Seq2Seq completed Baseline seq2seq + CNN based Architecture

--- models/conv_layers.py
+++ models/conv_layers.py
@@ -2,7 +2,7 @@ from tensorflow.python.keras import Sequential
 from tensorflow.python.keras.layers import Conv1D, MaxPooling1D
 
 
-def generate_cnn_layers(input_shape):
+def get_cnn_model(input_shape):
     """
     Creates a Sequential model with Conv and MaxPooling layers
     Prints summary of model
@@ -14,20 +14,4 @@ def generate_cnn_layers(input_shape):
     model.add(MaxPooling1D(4))
     model.add(Conv1D(128, 15, activation='relu'))
     model.add(MaxPooling1D(4))
-    print(model.summary())
     return model
-
-
-def return_cnn_layers(input_shape):
-    """
-    Returns list with Conv and MaxPooling layers to be used in another model
-    :param input_shape: tuple
-    :return: list
-    """
-    layers_list = []
-    layers_list.append(Conv1D(64, 15, activation='relu', input_shape=input_shape))
-    layers_list.append(MaxPooling1D(4))
-    layers_list.append(Conv1D(128, 15, activation='relu'))
-    layers_list.append(MaxPooling1D(4))
-    return layers_list
-
--- models/seq2seq.py
+++ models/seq2seq.py
@@ -1,48 +1,151 @@
-from tensorflow.python.keras.layers.recurrent import LSTM
+from .conv_layers import get_cnn_model
 from tensorflow.python.keras.layers import RepeatVector, TimeDistributed, Dense, Input, CuDNNLSTM
 from tensorflow.python.keras import Model
 
 
-MFCC_FEATURES = 40
-TARGET_LENGTH = 306
-latent_dim = 512
-# encoder training
+def generate_baseline_seq2seq_model(mfcc_features=40, target_length=40, latent_dim=512):
+    """
+    Encoder Decoder Architecture with Variable Size input and targets
+    :param mfcc_features: int
+    :param target_length: int
+    :param latent_dim: int
+    :return: Model
+    """
+    encoder_inputs = Input(shape=(None, mfcc_features))
+    encoder = CuDNNLSTM(latent_dim,
+                        batch_input_shape=(None, None, mfcc_features),
+                        stateful=False,
+                        return_state=True,
+                        recurrent_initializer='glorot_uniform')
 
-encoder_inputs = Input(shape=(None, ))
+    # 'encoder_outputs' are ignored and only states are kept.
+    encoder_outputs, state_h, state_c = encoder(encoder_inputs)
+    encoder_states = [state_h, state_c]
 
-encoder = CuDNNLSTM(latent_dim,
-                    batch_input_shape=(None, None, MFCC_FEATURES),
-                    stateful=False,
-                    return_state=True,
-                    recurrent_initializer='glorot_uniform')
+    # Decoder training, using 'encoder_states' as initial state.
+    decoder_inputs = Input(shape=(None, target_length))
+    # First Layer
+    decoder_lstm_1_layer = CuDNNLSTM(latent_dim,
+                                     batch_input_shape=(None, None, target_length),
+                                     stateful=False,
+                                     return_state=False,
+                                     dropout=0.2,
+                                     recurrent_dropout=0.2)
+    decoder_lstm1 = decoder_lstm_1_layer(decoder_inputs, initial_state=encoder_states)
+    # Second LSTM Layer
+    decoder_lstm_2_layer = CuDNNLSTM(latent_dim,
+                                     stateful=False,
+                                     return_sequences=True,
+                                     return_state=True,
+                                     dropout=0.2,
+                                     recurrent_dropout=0.2)
+    decoder_outputs, _, _ = decoder_lstm_2_layer(decoder_lstm1)
 
-# 'encoder_outputs' are ignored and only states are kept.
-encoder_outputs, state_h, state_c = encoder(encoder_inputs)
-encoder_states = [state_h, state_c]
+    # Dense Output Layers
+    decoder_dense = TimeDistributed(Dense(target_length, activation='softmax'))
+    decoder_outputs = decoder_dense(decoder_outputs)
 
-
-# Decoder training, using 'encoder_states' as initial state.
-decoder_inputs = Input(shape=(None, ))
-
-decoder_lstm_1 = CuDNNLSTM(latent_dim,
-                           batch_input_shape=(None, None, MFCC_FEATURES),
-                           stateful=False,
-                           return_state=False,
-                           dropout=0.2,
-                           recurrent_dropout=0.2)
-
-decoder_lstm_2 = CuDNNLSTM(latent_dim,
-                           stateful=False,
-                           return_sequences=True,
-                           return_state=True,
-                           dropout=0.2,
-                           recurrent_dropout=0.2)
-
-decoder_outputs, _, _ = decoder_lstm_2(decoder_lstm_1(decoder_inputs, initial_state = encoder_states))
-decoder_dense = TimeDistributed(Dense(TARGET_LENGTH, activation='relu'))
-decoder_outputs = decoder_dense(decoder_outputs)
+    # Generating Keras Model
+    model = Model([encoder_inputs, decoder_inputs], decoder_outputs)
+    return model
 
 
-# training model
-training_model = Model([encoder_inputs, decoder_inputs], decoder_outputs)
-training_model.compile(optimizer='adam', loss='mean_squared_error')
+def generate_attention_seq2seq_model(mfcc_features=40, target_length=40, latent_dim=512):
+    return 1
+
+
+def generate_cnn_attention_seq2seq_model(length, mfcc_features=40, target_length=40, latent_dim=512):
+    """
+      Encoder Decoder Architecture with CNN layers before LSTM.
+      Fixed Size Input For Conv Layers
+      Variable size Inputs and targets for LSTMs
+      :param mfcc_features: int
+      :param target_length: int
+      :param latent_dim: int
+      :return: Model
+      """
+    cnn_input_shape = (length, mfcc_features)
+    # getting CNN model
+    cnn_inputs = Input(shape=cnn_input_shape)
+    cnn_model = get_cnn_model(cnn_input_shape)
+    # Prepring Input shape for LSTM layer from CNN model
+    encoder_lstm_input_shape = (None, None, cnn_model.output_shape[2])
+    encoder_inputs = cnn_model(cnn_inputs)
+    encoder = CuDNNLSTM(latent_dim,
+                        batch_input_shape=encoder_lstm_input_shape,
+                        stateful=False,
+                        return_state=True,
+                        recurrent_initializer='glorot_uniform')
+
+    encoder_outputs, state_h, state_c = encoder(encoder_inputs)
+    encoder_states = [state_h, state_c]
+
+    # Decoder training, using 'encoder_states' as initial state.
+    decoder_inputs = Input(shape=(None, target_length))
+    # First Layer
+    decoder_lstm_1_layer = CuDNNLSTM(latent_dim,
+                                     batch_input_shape=(None, None, target_length),
+                                     stateful=False,
+                                     return_state=False,
+                                     dropout=0.2,
+                                     recurrent_dropout=0.2)
+    decoder_lstm1 = decoder_lstm_1_layer(decoder_inputs, initial_state=encoder_states)
+    # Second LSTM Layer
+    decoder_lstm_2_layer = CuDNNLSTM(latent_dim,
+                                     stateful=False,
+                                     return_sequences=True,
+                                     return_state=True,
+                                     dropout=0.2,
+                                     recurrent_dropout=0.2)
+    decoder_outputs, _, _ = decoder_lstm_2_layer(decoder_lstm1)
+
+    # Dense Output Layers
+    decoder_dense = TimeDistributed(Dense(target_length, activation='softmax'))
+    decoder_outputs = decoder_dense(decoder_outputs)
+
+    # Generating Keras Model
+    model = Model([encoder_inputs, decoder_inputs], decoder_outputs)
+    return model
+
+
+def train_model(encoder_input_data, decoder_input_data,decoder_target_data,
+                latent_dim=512, epochs=70, batch_size=64, model_architecture=1):
+    """
+    Choosing the architecture and running a training
+    :param encoder_input_data: Numpy 3dArray
+    :param decoder_input_data: Numpy 3dArray
+    :param decoder_target_data: Numpy 3dArray
+    :param latent_dim: int
+    :param epochs: int
+    :param batch_size: int
+    :param model_architecture: int
+    """
+
+    mfcc_features = encoder_input_data.shape[2]
+    target_length = decoder_input_data.shape[2]
+
+    if model_architecture == 1:
+        model = generate_baseline_seq2seq_model(mfcc_features=mfcc_features,
+                                                target_length=target_length,
+                                                latent_dim=latent_dim)
+    elif model_architecture == 2:
+        model = generate_attention_seq2seq_model(mfcc_features=mfcc_features,
+                                                 target_length=target_length,
+                                                 latent_dim=latent_dim)
+    else:
+        model = generate_cnn_attention_seq2seq_model(mfcc_features=mfcc_features,
+                                                     target_length=target_length,
+                                                     latent_dim=latent_dim)
+
+    model.compile(optimizer='rmsprop', loss='categorical_crossentropy')
+    model.fit([encoder_input_data, decoder_input_data], decoder_target_data,
+              batch_size=batch_size,
+              epochs=epochs,
+              validation_split=0.2)
+
+
+
+
+
+
+
