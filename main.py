f84ffb42d45384aeba37fbe2b33979670dcc3936
yasminehadjeres <yasmine.hadjeres@gmail.com>
2019-04-24 21:13:54 +0100

    added model architecture as in argument in the command line

--- main.py
+++ main.py
@@ -2,8 +2,9 @@ import sys
 from lib import upload_dataset
 from models import train_model, measure_test_accuracy
 
-batch_size = int(sys.argv[1])
-epochs = int(sys.argv[2])
+model = int(sys.argv[1])
+batch_size = int(sys.argv[2])
+epochs = int(sys.argv[3])
 
 (train_encoder_input, train_decoder_input, train_decoder_target), \
 (test_encoder_input, test_decoder_input, test_decoder_target) = upload_dataset()
@@ -16,7 +17,7 @@ model, encoder_states = train_model(encoder_input_data=train_encoder_input,
                                     decoder_target_data=train_decoder_target,
                                     batch_size=batch_size,
                                     epochs=epochs,
-                                    model_architecture=1)
+                                    model_architecture=model)
 
 
 # accuracy = measure_test_accuracy(test_decoder_input, model, encoder_states, latent_dim=512)
