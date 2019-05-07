331ead5c199e1f66e8a8ca6541d59698ebc0573c
sofianeMdjk <sofiane.mdjk@gmail.com>
2019-05-07 12:33:44 +0100

    Bug on saving the model

--- models/model_callback.py
+++ models/model_callback.py
@@ -8,13 +8,15 @@ class ModelSaver(Callback):
         self.model_name = model_name
         self.model_path = model_path
         self.drive_instance = drive_instance
-        if self.model is not None :
-            self.model.save(model_path)
-        else:
-            print("model is None")
 
     def on_epoch_end(self, epoch, logs=None):
-        model_title = self.model_name.split(".h5")[0]
-        uploaded = self.drive_instance.CreateFile({model_title: self.model_name})
-        uploaded.SetContentFile(self.model_path)
-        uploaded.Upload()
+        if self.model is not None :
+            print("model available")
+            self.model.save(self.model_path)
+            model_title = self.model_name.split(".h5")[0]
+            uploaded = self.drive_instance.CreateFile({model_title: self.model_name})
+            uploaded.SetContentFile(self.model_path)
+            uploaded.Upload()
+
+        else:
+            print("model is None")
