c56007fb8f6d66a7b82ba677fd0211c7e9586f2c
sofianeMdjk <sofiane.mdjk@gmail.com>
2019-05-07 12:52:34 +0100

    Changed save path in Google Drive

--- models/model_callback.py
+++ models/model_callback.py
@@ -10,13 +10,10 @@ class ModelSaver(Callback):
         self.drive_instance = drive_instance
 
     def on_epoch_end(self, epoch, logs=None):
-        if self.model is not None :
-            print("model available")
-            self.model.save(self.model_path)
-            model_title = self.model_name.split(".h5")[0]
-            uploaded = self.drive_instance.CreateFile({model_title: self.model_name})
-            uploaded.SetContentFile(self.model_path)
-            uploaded.Upload()
+        self.model.save(self.model_path)
+        model_title = self.model_name.split(".h5")[0]
+        parent_directory_id = '0B5fJkPjHLj3Jdkw5ZnFiY0lZV1U'
+        uploaded = self.drive_instance.CreateFile({model_title: self.model_name, "parents": [{"kind": "drive#fileLink", "id": parent_directory_id}]})
+        uploaded.SetContentFile(self.model_path)
+        uploaded.Upload()
 
-        else:
-            print("model is None")
