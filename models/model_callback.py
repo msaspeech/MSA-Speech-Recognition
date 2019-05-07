d7f24c9fc636ef828186f70b49b4eda0d116ddd2
sofianeMdjk <sofiane.mdjk@gmail.com>
2019-05-07 11:49:21 +0100

    Bug on saving the model

--- models/model_callback.py
+++ models/model_callback.py
@@ -4,12 +4,11 @@ from keras.callbacks import Callback
 class ModelSaver(Callback):
 
     def __init__(self, model_name, model_path, drive_instance):
-        super(ModelSaver, self).__init__()
+        super().__init__()
         self.model_name = model_name
         self.model_path = model_path
         self.drive_instance = drive_instance
-        #self.model.save(model_path)
-        super(ModelSaver, self).model.save(model_path)
+        self.model.save(model_path)
 
     def on_epoch_end(self, epoch, logs=None):
         model_title = self.model_name.split(".h5")[0]
