7bf63ddc78b2d09838935b41369de43e94c1730c
sofianeMdjk <sofiane.mdjk@gmail.com>
2019-05-13 14:36:42 +0100

    Updated cnn input shape and removed normalization

--- models/seq2seq_cnn_attention.py
+++ models/seq2seq_cnn_attention.py
@@ -17,9 +17,9 @@ def train_cnn_seq2seq_model(audio_length, mfcc_features, target_length, batch_si
     """
     cnn_input_shape = (audio_length, mfcc_features)
     # getting CNN model
-    cnn_inputs = Input(shape=cnn_input_shape, name="encoder_inputs")
     cnn_model = get_cnn_model(cnn_input_shape)
-
+    cnn_model_output_shape = cnn_model.layers[-1].output_shape[2]
+    cnn_inputs = Input(shape=(None, cnn_model_output_shape), name="encoder_inputs")
     # Preparing Input shape for LSTM layer from CNN model
     cnn_output = cnn_model(cnn_inputs)
     encoder_states = get_encoder_states(mfcc_features=mfcc_features,
