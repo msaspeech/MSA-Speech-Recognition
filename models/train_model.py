1c29b0d9ca64241e63ef1131c152adc6650e4c4c
sofianeMdjk <sofiane.mdjk@gmail.com>
2019-05-24 16:07:01 +0100

    evaluating test data

--- models/train_model.py
+++ models/train_model.py
@@ -33,7 +33,8 @@ class Seq2SeqModel():
     def _load_model(self):
         if file_exists(self.model_path):
             self.model = models.load_model(self.model_path)
-            self.model.evaluate_generator(self.validation_generator(), steps= settings.TOTAL_SAMPLES_NUMBER, verbose=1)
+          
+            self.model.evaluate_generator(self.validation_generator(), steps=settings.TOTAL_SAMPLES_NUMBER, verbose=1)
             print(self.model.summary())
         else:
             if self.model_architecture == 1:
