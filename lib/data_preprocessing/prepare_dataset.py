329a76b508cddedb9d47ef994b2fbe22a2b4af73
sofianeMdjk <sofiane.mdjk@gmail.com>
2019-05-24 17:31:45 +0100

    Modified word set

--- lib/data_preprocessing/prepare_dataset.py
+++ lib/data_preprocessing/prepare_dataset.py
@@ -1,4 +1,4 @@
-from utils import  get_character_set, get_distinct_words, empty_directory
+from utils import  get_character_set, get_distinct_words, empty_directory, get_empty_binary_vector
 from . import generate_decoder_input_target, normalize_encoder_input
 import numpy as np
 from etc import settings
@@ -129,10 +129,12 @@ def upload_dataset(train_ratio=0.8, padding=False, word_level=False, partitions=
         _generate_spllited_encoder_input_data(test_audio, test=True,partitions=partitions)
 
         if word_level:
+
             distinct_words = get_distinct_words(transcripts=all_transcripts)
+            settings.WORD_SET = len(get_empty_binary_vector(len(distinct_words)))
             #generate_pickle_file(distinct_words, file_path=settings.DISTINCT_WORDS_PATH)
-            general_info.append(distinct_words)
-            settings.WORD_SET = distinct_words
+            general_info.append(settings.WORD_SET)
+            #settings.WORD_SET = distinct_words
         else:
             distinct_characters = get_character_set(transcripts=all_transcripts)
             #generate_pickle_file(distinct_characters, file_path=settings.DISTINCT_CHARACTERS_PATH)
