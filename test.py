96b5650d9de5d4f8705f107644e6d0a15a76d786
sofianeMdjk <sofiane.mdjk@gmail.com>
2019-05-27 15:48:20 +0100

    Testing accuracy

--- test.py
+++ test.py
@@ -8,23 +8,27 @@ infos = load_pickle_data(settings.DATASET_INFORMATION_PATH)
 settings.CHARACTER_SET = infos[3]
 char_to_int = convert_to_int(settings.CHARACTER_SET)
 
-model = models.load_model("trained_models/architecture1.h5")
 sample = AudioInput("test.wav", "")
 print(sample.mfcc.transpose())
 audio = [sample.mfcc.transpose()]
 audio_sequence = np.array(audio)
-
+target_sequence = np.zeros((1, 15, 882))
 transcript = "SOS_ lr}Asp Aljmhwryp fy syAq AlAntxAbAt Alr}Asyp Almqblp lmA*A h*A AlqrAr fy Alwqt Al*y _EOS"
 encoded_word = [0]*882
 encoded_transcript = []
-target_sequence = np.array([None])
-for word_index, word in enumerate(transcript):
+#target_sequence = np.array([None])
+for word_index, word in enumerate(transcript.split()):
+    #print(word_index)
     for index_character, character in enumerate(word):
         index = char_to_int[character] + (index_character * len(settings.CHARACTER_SET))
-        encoded_word[index] = 1
-    encoded_transcript.append(encoded_word)
+        target_sequence[0, word_index, index] = 1
+        #encoded_word[index] = 1
+    #encoded_transcript.append(encoded_word)
 
-target_sequence[0] = encoded_transcript
 
+#target_sequence[0] = encoded_transcript
+print(target_sequence.shape)
+
+model = models.load_model("trained_models/architecture1.h5")
 output = model.predict([audio, target_sequence])
 print(output)
