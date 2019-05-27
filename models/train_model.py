8fee69d4fbfec6060fbadb01323255f08035b0ba
sofianeMdjk <sofiane.mdjk@gmail.com>
2019-05-27 17:07:19 +0100

    using binary accuracy as metric

--- models/train_model.py
+++ models/train_model.py
@@ -74,7 +74,7 @@ class Seq2SeqModel():
     def train_model(self):
         print("ENCODER STATES")
         if self.word_level:
-            self.model.compile(optimizer='rmsprop', loss='mse', metrics=['accuracy'])
+            self.model.compile(optimizer='rmsprop', loss='mse', metrics=['binary_accuracy'])
         else:
             self.model.compile(optimizer='rmsprop', loss='categorical_crossentropy', metrics=['accuracy'])
         model_saver = ModelSaver(model_name=self.model_name, model_path=self.model_path,
