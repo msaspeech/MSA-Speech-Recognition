10e22045452b1c54d2bebbc67d79bce99bbfcf5b
sofianeMdjk <sofiane.mdjk@gmail.com>
2019-05-27 15:30:51 +0100

    Testing accuracy

--- test.py
+++ test.py
@@ -7,16 +7,17 @@ from etc import settings
 infos = load_pickle_data(settings.DATASET_INFORMATION_PATH)
 settings.CHARACTER_SET = infos[3]
 char_to_int = convert_to_int(settings.CHARACTER_SET)
-transcript = "SOS_ lr}Asp Aljmhwryp fy syAq AlAntxAbAt Alr}Asyp Almqblp lmA*A h*A AlqrAr fy Alwqt Al*y _EOS"
-#model = models.load_model("trained_models/architecture1.h5")
+
+model = models.load_model("trained_models/architecture1.h5")
 sample = AudioInput("test.wav", "")
 print(sample.mfcc.transpose())
 audio = [sample.mfcc.transpose()]
 audio_sequence = np.array(audio)
 
+transcript = "SOS_ lr}Asp Aljmhwryp fy syAq AlAntxAbAt Alr}Asyp Almqblp lmA*A h*A AlqrAr fy Alwqt Al*y _EOS"
 encoded_word = [0]*882
 encoded_transcript = []
-target_sequence = np.zero([None])
+target_sequence = np.array([None])
 for word_index, word in enumerate(transcript):
     for index_character, character in enumerate(word):
         index = char_to_int[character] + (index_character * len(settings.CHARACTER_SET))
