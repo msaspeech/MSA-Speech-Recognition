6951d3dd2550dde45a6b41ec38f8233909d7fb30
sofianeMdjk <sofiane.mdjk@gmail.com>
2019-06-13 21:26:05 +0100

    Testing character level

--- main_test_model.py
+++ main_test_model.py
@@ -5,21 +5,27 @@ from etc import settings
 from utils import load_pickle_data
 
 
-general_info = load_pickle_data(settings.DATASET_WORD_INFORMATION_PATH)
-settings.MFCC_FEATURES_LENGTH = general_info[0]
-settings.TOTAL_SAMPLES_NUMBER = general_info[1]
-settings.WORD_SET = general_info[2]
-settings.LONGEST_WORD_LENGTH = general_info[3]
-settings.CHARACTER_SET = general_info[4]
-settings.WORD_TARGET_LENGTH = general_info[5]
 
-x = int(settings.TOTAL_SAMPLES_NUMBER * 95/100)
-print(x)
 word_level = int(sys.argv[1])
 architecture = int(sys.argv[2])
 latent_dim = int(sys.argv[3])
 epochs = int(sys.argv[4])
 
+
+if word_level:
+    general_info = load_pickle_data(settings.DATASET_WORD_INFORMATION_PATH)
+    settings.MFCC_FEATURES_LENGTH = general_info[0]
+    settings.TOTAL_SAMPLES_NUMBER = general_info[1]
+    settings.WORD_SET = general_info[2]
+    settings.LONGEST_WORD_LENGTH = general_info[3]
+    settings.CHARACTER_SET = general_info[4]
+    settings.WORD_TARGET_LENGTH = general_info[5]
+
+else:
+    general_info = load_pickle_data(settings.DATASET_CHAR_INFORMATION_PATH)
+    settings.MFCC_FEATURES_LENGTH = general_info[0]
+    settings.TOTAL_SAMPLES_NUMBER = general_info[1]
+    settings.CHARACTER_SET = general_info[2]
 
 model = Seq2SeqModel(latent_dim=latent_dim, epochs=epochs, model_architecture=architecture, word_level=word_level)
 model.test_model()
