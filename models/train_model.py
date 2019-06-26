d2410a51e6d1481d30de000879f6ac27c33d3e90
sofianeMdjk <sofiane.mdjk@gmail.com>
2019-06-26 18:56:31 +0100

    fixed google drive problem

--- models/train_model.py
+++ models/train_model.py
@@ -138,7 +138,7 @@ class Seq2SeqModel():
             batch_size = 32
             steps = int(settings.TOTAL_SAMPLES_NUMBER / batch_size) + 1
             history = self.model.fit_generator(self.split_data_generator_dict(batch_size),
-                                               steps_per_epoch=500,
+                                               steps_per_epoch=steps,
                                                epochs=self.epochs,
                                                callbacks=[model_saver])
 
