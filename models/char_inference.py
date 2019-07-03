52d3984e6d9055ccfcc6310bf4553c37396ebbb4
sofianeMdjk <sofiane.mdjk@gmail.com>
2019-07-03 23:33:52 +0100

    displaying summary

--- models/char_inference.py
+++ models/char_inference.py
@@ -124,12 +124,13 @@ class Char_Inference():
         decoder_state_input_h3 = Input(shape=(self.latent_dim,))
         decoder_state_input_h4 = Input(shape=(self.latent_dim,))
 
-        decoder_states_inputs = [decoder_state_input_h1, decoder_state_input_h2,
-                                 decoder_state_input_h3, decoder_state_input_h4]
+        decoder_layers_initial_states = np.zeros((self.latent_dim, ))
+
+        decoder_states_inputs = [decoder_state_input_h1, decoder_layers_initial_states,
+                                 decoder_layers_initial_states, decoder_layers_initial_states]
 
         decoder_gru1, state_h1 = decoder_gru1_layer(decoder_inputs, initial_state=decoder_state_input_h1)
 
-        decoder_layers_initial_states = np.zeros((self.latent_dim, ))
 
         decoder_gru2, state_h2 = decoder_gru2_layer(decoder_gru1, initial_state=decoder_state_input_h1)
         decoder_gru3, state_h3 = decoder_gru3_layer(decoder_gru2, initial_state=decoder_state_input_h1)
