df82d947fbd7107934a272c4fa8509d1b4f2f374
sofianeMdjk <sofiane.mdjk@gmail.com>
2019-07-04 11:16:23 +0100

    rectified inference

--- models/char_inference.py
+++ models/char_inference.py
@@ -131,9 +131,9 @@ class Char_Inference():
 
         #decoder_layers_initial_states = np.zeros((self.latent_dim, ))
 
-        decoder_gru2, state_h2 = decoder_gru2_layer(decoder_gru1, initial_state=decoder_state_input_h1)
-        decoder_gru3, state_h3 = decoder_gru3_layer(decoder_gru2, initial_state=decoder_state_input_h1)
-        decoder_output, state_h4 = decoder_gru4_layer(decoder_gru3, initial_state=decoder_state_input_h1)
+        decoder_gru2, state_h2 = decoder_gru2_layer(decoder_gru1, initial_state=decoder_state_input_h2)
+        decoder_gru3, state_h3 = decoder_gru3_layer(decoder_gru2, initial_state=decoder_state_input_h3)
+        decoder_output, state_h4 = decoder_gru4_layer(decoder_gru3, initial_state=decoder_state_input_h4)
 
         decoder_states = [state_h1, state_h2,state_h3,state_h4]
 
