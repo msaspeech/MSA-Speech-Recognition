225ee5a7aadf9bb7daf6fa79dbccc6d96134b8a0
sofianeMdjk <sofiane.mdjk@gmail.com>
2019-07-05 17:49:41 +0100

    Changing model path

--- test2.py
+++ test2.py
@@ -1,5 +1,5 @@
-from keras import models
+from tensorflow.python.keras import models
 
-model = models.load_model("./model.h5")
+model = models.load_model("model.h5")
 model.summary()
 model.save_weights("model_weights.h5")
