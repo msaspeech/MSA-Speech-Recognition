3d06e5f31ca34eeb2aea3c5e30f51fc19de23d94
sofianeMdjk <sofiane.mdjk@gmail.com>
2019-07-05 23:40:16 +0100

    running new learning

--- test.py
+++ test.py
@@ -8,59 +8,33 @@ def predict_sequence_test(audio_input):
     char_to_int = convert_to_int(sorted(settings.CHARACTER_SET))
     int_to_char = convert_int_to_char(char_to_int)
 
-    t_force = "SOS_ wbyn sEy AlqwAt AlmslHp Alty kAn ytHdv bAsmhA _EOS"
-    words = t_force.split()
-    # print(words)
-    character_set_length = 50
-    dec_input_len = 800
+    t_force = "\twbyn sEy AlqwAt AlmslHp Alty kAn ytHdv bAsmhA "
     encoded_transcript = []
-    for word in words:
-        encoded_word = [0] * dec_input_len
-        i = 0
-        for i, character in enumerate(word):
-            position = character_set_length * i + char_to_int[character]
-            encoded_word[position] = 1
-            # print(position)
+    for index, character in enumerate(t_force):
+        encoded_character = [0] * len(settings.CHARACTER_SET)
+        position = char_to_int[character]
+        encoded_character[position] = 1
+        encoded_transcript.append(encoded_character)
 
-        if i < 16 - 1:
-            for k in range(i + 1, 16):
-                position = character_set_length * k + character_set_length - 1
-                encoded_word[position] = 1
-
-        encoded_transcript.append(encoded_word)
-
-    transcript = np.zeros((1, len(words), dec_input_len))
-    transcript[0] = encoded_transcript
-
-    # print(transcript.shape)
-    # print(transcript[0][0])
-    # print([np.argmax(transcript[0][0][i:i+41]) for i in range(0, 574, 41)])
+    decoder_input = np.array([encoded_transcript])
+    print(decoder_input.shape)
     model = models.load_model("model.h5")
-    result = model.predict([audio_input, transcript])
+    output = model.predict([audio_input, decoder_input])
+    print(output.shape)
+    sentence = ""
+    output = output[0]
+    for character in output:
+        position = np.argmax(character)
+        character = int_to_char[position]
+        sentence += character
 
-    list_words = [""] * len(words)
-    for i, characters in enumerate(result):
-        encoded_characters = characters[0]
-        for i, encoded_character in enumerate(encoded_characters):
-            index = np.argmax(np.array(encoded_character))
-            if index == character_set_length - 1:
-                character = ""
-            else:
-                character = int_to_char[index]
-            list_words[i] += character
+    print(sentence)
 
-    print(list_words)
-
-
-general_info = load_pickle_data("info_word.pkl")
+general_info = load_pickle_data("info_char.pkl")
 settings.MFCC_FEATURES_LENGTH = general_info[0]
-settings.TOTAL_SAMPLES_NUMBER = general_info[1]
-settings.WORD_SET = general_info[2]
-settings.LONGEST_WORD_LENGTH = general_info[3]
-settings.CHARACTER_SET = general_info[4]
-settings.WORD_TARGET_LENGTH = general_info[5]
+settings.CHARACTER_SET = general_info[2]
 
-sample = AudioInput("test.wav", "")
+sample = AudioInput("track.wav", "")
 audio = [sample.mfcc.transpose()]
 
 audio_sequence = np.array(audio, dtype=np.float32)
--- test2.py
+++ test2.py
@@ -2,6 +2,7 @@ from tensorflow.python.keras import models, Input, Model
 from tensorflow.python.keras.layers import Dropout, Dense, GRU
 from etc import settings
 
+
 def get_multi_output_dense(decoder_outputs, target_length):
     dense_layers = []
 
@@ -14,15 +15,15 @@ def get_multi_output_dense(decoder_outputs, target_length):
 
 def get_encoder_states(input_shape, encoder_inputs, latent_dim, return_sequences=False):
     encoder = GRU(latent_dim,
-                        input_shape=(None, input_shape),
-                        stateful=False,
-                        return_sequences=return_sequences,
-                        return_state=True,
-                        kernel_constraint=None,
-                        kernel_regularizer=None,
-                        reset_after=True,
-                        recurrent_initializer='glorot_uniform',
-                        name="encoder_gru_layer")
+                  input_shape=(None, input_shape),
+                  stateful=False,
+                  return_sequences=return_sequences,
+                  return_state=True,
+                  kernel_constraint=None,
+                  kernel_regularizer=None,
+                  reset_after=True,
+                  recurrent_initializer='glorot_uniform',
+                  name="encoder_gru_layer")
     # 'encoder_outputs' are ignored and only states are kept.
     encoder_outputs, state_h = encoder(encoder_inputs)
     encoder_states = [state_h]
@@ -35,79 +36,69 @@ def get_encoder_states(input_shape, encoder_inputs, latent_dim, return_sequences
 def get_decoder_outputs(target_length, encoder_states, decoder_inputs, latent_dim):
     # First Layer
     decoder_gru1_layer = GRU(latent_dim,
-                                    input_shape=(None, target_length),
-                                    return_sequences=True,
-                                    return_state=True,
-                                    reset_after=True,
-                                    kernel_constraint=None,
-                                    kernel_regularizer=None,
-                                    name="decoder_gru1_layer")
+                             input_shape=(None, target_length),
+                             return_sequences=True,
+                             return_state=True,
+                             reset_after=True,
+                             kernel_constraint=None,
+                             kernel_regularizer=None,
+                             name="decoder_gru1_layer")
     decoder_gru1, state_h = decoder_gru1_layer(decoder_inputs, initial_state=encoder_states)
 
     # Second LSTM Layer
     decoder_gru2_layer = GRU(latent_dim,
-                                    stateful=False,
-                                    return_sequences=True,
-                                    return_state=True,
-                                    reset_after=True,
-                                    kernel_constraint=None,
-                                    kernel_regularizer=None,
-                                    name="decoder_gru2_layer")
-    decoder_gru2, state_h = decoder_gru2_layer(decoder_gru1, initial_state=state_h)
+                             stateful=False,
+                             return_sequences=True,
+                             return_state=False,
+                             reset_after=True,
+                             kernel_constraint=None,
+                             kernel_regularizer=None,
+                             name="decoder_gru2_layer")
+    decoder_gru2 = decoder_gru2_layer(decoder_gru1)
 
     decoder_gru3_layer = GRU(latent_dim,
-                                  stateful=False,
-                                  return_sequences=True,
-                                  return_state=True,
-                                  reset_after=True,
-                                  kernel_constraint=None,
-                                  kernel_regularizer=None,
-                                  name="decoder_gru3_layer")
-    decoder_gru3, state_h = decoder_gru3_layer(decoder_gru2, initial_state=state_h)
-
-    decoder_gru4_layer = GRU(latent_dim,
-                                  stateful=False,
-                                  return_sequences=True,
-                                  return_state=True,
-                                  reset_after=True,
-                                  kernel_constraint=None,
-                                  kernel_regularizer=None,
-                                  name="decoder_gru4_layer")
-    decoder_outputs, state_h = decoder_gru4_layer(decoder_gru3, initial_state=state_h)
-
+                             stateful=False,
+                             return_sequences=True,
+                             return_state=False,
+                             reset_after=True,
+                             kernel_constraint=None,
+                             kernel_regularizer=None,
+                             name="decoder_gru3_layer")
+    decoder_outputs = decoder_gru3_layer(decoder_gru2)
 
     return decoder_outputs
 
 
-model = models.load_model("model.h5")
-model.summary()
-model.save_weights("model_weights.h5")
+# model = models.load_model("model.h5")
+# model.summary()
+# model.save_weights("model_weights.h5")
 
 encoder_inputs = Input(shape=(None, 40), name="encoder_input")
 
 # Encoder model
 encoder_states = get_encoder_states(input_shape=40,
-                                        encoder_inputs=encoder_inputs,
-                                        latent_dim=350)
+                                    encoder_inputs=encoder_inputs,
+                                    latent_dim=600)
 
 # Decoder training, using 'encoder_states' as initial state.
-decoder_inputs = Input(shape=(None, 800), name="decoder_input")
+decoder_inputs = Input(shape=(None, 49), name="decoder_input")
 
-decoder_outputs = get_decoder_outputs(target_length=50,
+decoder_outputs = get_decoder_outputs(target_length=49,
                                       encoder_states=encoder_states,
-                                          decoder_inputs=decoder_inputs,
-                                          latent_dim=350)
+                                      decoder_inputs=decoder_inputs,
+                                      latent_dim=600)
 
 dropout_layer = Dropout(0.5, name="decoder_dropout")
 decoder_outputs = dropout_layer(decoder_outputs)
 
 # Dense Output Layers
 
-target_length = 50
-decoder_outputs = get_multi_output_dense(decoder_outputs, target_length)
+target_length = 49
+decoder_dense = Dense(target_length, activation='softmax', name="decoder_dense")
+decoder_outputs = decoder_dense(decoder_outputs)
+
 # Generating Keras Model
 model = Model([encoder_inputs, decoder_inputs], decoder_outputs)
 
-#model.load_weights("model_weights.h5")
-#model.save("model.h5")
-
+model.load_weights("model_weights.h5")
+model.save("model.h5")
