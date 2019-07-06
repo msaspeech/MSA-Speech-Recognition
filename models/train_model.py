f73db396858711c8802e4cdc2e29795aa6cf8258
sofianeMdjk <sofiane.mdjk@gmail.com>
2019-07-06 13:24:59 +0100

    SGD

--- models/train_model.py
+++ models/train_model.py
@@ -134,7 +134,7 @@ class Seq2SeqModel():
 
         else:
             print("training here" )
-            self.model.compile(optimizer="Adagrad", loss='categorical_crossentropy', metrics=['accuracy'])
+            self.model.compile(optimizer="sgd", loss='categorical_crossentropy', metrics=['accuracy'])
 
             batch_size = 32
             steps = int(settings.TOTAL_SAMPLES_NUMBER / batch_size) + 1
